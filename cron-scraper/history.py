28a8d17b2253ea733597e9be337b04a3fd29d09a
Posiitron <posiitronnet@gmail.com>
2023-08-28 14:01:50 +0200

    data history added

---
+++ cron-scraper/history.py
@@ -0,0 +1,204 @@
+from datetime import datetime
+import json
+import logging
+import os
+import csv
+import time
+import requests
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+def calculate_average(numbers):
+    if len(numbers) == 0:
+        return None
+    return sum(numbers) / len(numbers)
+
+def extract_data_from_file(file_path):
+    extracted_data = {
+        "Temperature": "none",
+        "Pressure": "none",
+        "Humidity": "none",
+        "PM2_5": "none",
+        "PM10": "none"
+    }
+
+    with open(file_path, "r") as file:
+        reader = csv.reader(file)
+        header = next(reader)
+
+        if "Sensor Code" in header:
+            for parts in reader:
+                if len(parts) >= 7 and parts[0] != "Sensor Code":
+                    _, so2, _, co, o3, pm10, pm2_5 = parts
+                    extracted_data["PM2_5"] = pm2_5.strip() or extracted_data["PM2_5"]
+                    extracted_data["PM10"] = pm10.strip() or extracted_data["PM10"]
+        elif "Temperature" in header:
+            for parts in reader:
+                if len(parts) >= 5 and parts[0] != "Temperature":
+                    temperature, pressure, humidity, pm25, pm10 = parts
+                    extracted_data["Temperature"] = temperature.strip() or extracted_data["Temperature"]
+                    extracted_data["Pressure"] = pressure.strip() or extracted_data["Pressure"]
+                    extracted_data["Humidity"] = humidity.strip() or extracted_data["Humidity"]
+                    extracted_data["PM2_5"] = pm25.strip() or extracted_data["PM2_5"]
+                    extracted_data["PM10"] = pm10.strip() or extracted_data["PM10"]
+
+    return extracted_data
+
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=2, max=10))
+def fetch_temperature_data(location):
+    open_weather_api_key = "ce48301aaf1d16612230648eeff30329"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={open_weather_api_key}&units=metric"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        logger.info("OpenWeatherMap API Response:", data)  # log the response for debugging
+        if "main" in data:
+            return data["main"]
+        else:
+            logger.info("Response does not contain 'main' key.")
+    else:
+        logger.error("Failed to fetch temperature data. Status code:", response.status_code)
+    
+    return None
+
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=2, max=10))
+def fetch_air_quality_data(latitude, longitude):
+    open_weather_api_key = "ce48301aaf1d16612230648eeff30329"
+    url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={latitude}&lon={longitude}&appid={open_weather_api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()["list"][0]["components"]
+    return data
+
+def fetch_data_for_all_files():
+    folder_paths = [
+        "data/sensor_community",
+        "data/CHMU/Česká republika"
+    ]
+    file_extension = ".csv"
+
+    excluded_filenames = ["all-sensors.csv", "all_stations.csv"]  # Add filenames to exclude
+
+    location = "Czech Republic"
+    temperature_data = fetch_temperature_data(location)
+    if not temperature_data:
+        logger.info("Failed to fetch temperature data.")
+        return []
+
+    latitude = 50.0755
+    longitude = 14.4378
+    air_quality_data = fetch_air_quality_data(latitude, longitude)
+    if not air_quality_data:
+        logger.error("Failed to fetch air quality data.")
+        return []
+
+    average_temperature = calculate_average([temperature_data["temp"]])
+    average_pressure = calculate_average([temperature_data["pressure"]])
+    average_humidity = calculate_average([temperature_data["humidity"]])
+    average_pm2_5 = calculate_average([air_quality_data["pm2_5"]])
+    average_pm10 = calculate_average([air_quality_data["pm10"]])
+
+    final_data = []
+
+    for folder_path in folder_paths:
+        file_names = os.listdir(folder_path)
+
+        for file_name in file_names:
+            if file_name in excluded_filenames:
+                continue  # Skip processing excluded filenames
+
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.isfile(file_path) and file_name.lower().endswith(file_extension):
+                logger.info(f"Processing file: {file_name}")
+                location_address = os.path.splitext(file_name)[0]
+                extracted_data = extract_data_from_file(file_path)
+
+                open_weather_data = fetch_temperature_data(location_address)
+
+                if open_weather_data:
+                    if extracted_data["Temperature"] == "none":
+                        extracted_data["Temperature"] = open_weather_data["temp"]
+                    if extracted_data["Pressure"] == "none":
+                        extracted_data["Pressure"] = open_weather_data["pressure"]
+                    if extracted_data["Humidity"] == "none":
+                        extracted_data["Humidity"] = open_weather_data["humidity"]
+
+                def update_value(value, average_value):
+                    if isinstance(value, str) and value != "none" and value.replace(".", "", 1).isdigit():
+                        return float(value)
+                    return average_value
+
+
+                extracted_data["Temperature"] = update_value(extracted_data["Temperature"], average_temperature)
+                extracted_data["Pressure"] = update_value(extracted_data["Pressure"], average_pressure)
+                extracted_data["Humidity"] = update_value(extracted_data["Humidity"], average_humidity)
+                extracted_data["PM2_5"] = update_value(extracted_data["PM2_5"], average_pm2_5)
+                extracted_data["PM10"] = update_value(extracted_data["PM10"], average_pm10)
+
+                final_extracted_data = {
+                    "Location": location_address,
+                    "Temperature": extracted_data["Temperature"],
+                    "Pressure": extracted_data["Pressure"],
+                    "Humidity": extracted_data["Humidity"],
+                    "PM2_5": extracted_data["PM2_5"],
+                    "PM10": extracted_data["PM10"]
+                }
+
+                logger.info(f"Final extracted data for {file_name}:", final_extracted_data)
+                final_data.append(final_extracted_data)
+
+            time.sleep(1)
+
+    return final_data
+
+if __name__ == "__main__":
+    try:
+        log_path = 'logs/historylog.txt'
+        log_file_path = os.path.join(log_path, 'historylog.txt')
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setLevel(logging.INFO)
+        file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+        
+        logger.info("Starting the script...")
+
+        data = fetch_data_for_all_files()
+
+        # Get the current date and time
+        current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        
+        # Create the 'data/history/region_data' folder if it doesn't exist
+        output_folder = os.path.join("data", "history", "region_data")
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        # Save the collected data as a JSON file
+        output_filename = os.path.join(output_folder, f"data_{current_datetime}.json")
+        with open(output_filename, "w", encoding="utf-8") as output_file:
+            json.dump(data, output_file, indent=4, ensure_ascii=False)
+
+        logger.info(f"Data saved to {output_filename}")
+
+        logger.info("Script execution completed.")
+    except Exception as e:
+        logger.exception("An error occurred during script execution: %s", e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utils/search-location.js
+++ utils/search-location.js
@@ -1,253 +1,98 @@
 const fs = require("fs");
 const path = require("path");
 const Fuse = require("fuse.js");
-const axios = require("axios"); // Import Axios for making API requests
 
-// Function to calculate the average of an array of numbers
-function calculateAverage(numbers) {
-  if (numbers.length === 0) {
-    return null;
+// Function to perform the search using Fuse.js
+function performSearch(query, data, numResults) {
+  const options = {
+    keys: ["Location"], // The field to search in
+    includeScore: true,
+    shouldSort: true,
+    threshold: 0.3, // Adjust the threshold for better matching
+    minMatchCharLength: 3, // Require at least 2 characters for matching
+  };
+  const fuse = new Fuse(data, options);
+
+  // Perform the search with the original query and the exact match query
+  const searchResults = fuse.search(query).concat(fuse.search(`"${query}"`));
+
+  // Deduplicate and extract the N most relevant results
+  const deduplicatedResults = [];
+  const seenLocations = new Set();
+  for (const result of searchResults) {
+    if (!seenLocations.has(result.item.Location)) {
+      deduplicatedResults.push(result.item);
+      seenLocations.add(result.item.Location);
+      if (deduplicatedResults.length >= numResults) {
+        break;
+      }
+    }
   }
-  const sum = numbers.reduce((acc, num) => acc + num, 0);
-  return sum / numbers.length;
+
+  return deduplicatedResults;
 }
 
-function searchFiles(
-  query,
-  folderPaths,
-  fileExtension,
-  numResults,
-  excludedStrings
-) {
-  let allFileNames = [];
+// Function to read the JSON data from a file
+function readJSONData(filePath) {
+  try {
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error("Error reading JSON data:", error);
+    return null;
+  }
+}
 
-  // Iterate over each folder path
-  for (const folderPath of folderPaths) {
-    // Read the list of files in the folder
+// Function to get the newest JSON file from a folder
+function getNewestJSONFile(folderPath) {
+  try {
     const fileNames = fs.readdirSync(folderPath);
 
-    // Filter file names based on the provided file extension
-    const filteredFileNames = fileNames.filter(
-      (fileName) =>
-        path.extname(fileName).toLowerCase() === fileExtension.toLowerCase() &&
-        !excludedStrings.some((excludedStr) =>
-          fileName.toLowerCase().includes(excludedStr.toLowerCase())
-        )
+    // Filter only JSON files
+    const jsonFileNames = fileNames.filter(
+      (fileName) => path.extname(fileName) === ".json"
     );
 
-    allFileNames = allFileNames.concat(filteredFileNames);
-  }
+    // Get file creation timestamps
+    const fileStats = jsonFileNames.map((fileName) => {
+      const filePath = path.join(folderPath, fileName);
+      return { fileName, createdAt: fs.statSync(filePath).birthtime };
+    });
 
-  // Create a Fuse instance with custom options
-  const options = {
-    keys: ["fileName"], // The field to search in
-    shouldSort: true,
-    includeScore: true,
-    threshold: 0.4, // Adjust the threshold for matching
-  };
-  const fuse = new Fuse(
-    allFileNames.map((fileName) => ({ fileName })),
-    options
-  );
+    // Sort files based on creation timestamps
+    fileStats.sort((a, b) => b.createdAt - a.createdAt);
 
-  // Perform the search
-  const searchResults = fuse.search(query);
-
-  // Extract and return the N most relevant file names
-  const topResults = searchResults
-    .slice(0, numResults)
-    .map((item) => item.item.fileName);
-  return topResults;
-}
-
-const openWeatherApiKey = "ce48301aaf1d16612230648eeff30329";
-
-// Function to fetch temperature data from OpenWeatherMap API
-async function fetchTemperatureData(location) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
-    location
-  )}&appid=${openWeatherApiKey}&units=metric`;
-
-  try {
-    const response = await axios.get(url);
-    return response.data.main;
-  } catch (error) {
-    console.error("Error fetching temperature data:", error);
-    return null;
-  }
-}
-
-// Function to fetch air quality data from OpenWeatherMap API
-async function fetchAirQualityData(latitude, longitude) {
-  const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=${openWeatherApiKey}&units=metric`;
-
-  try {
-    const response = await axios.get(url);
-    return response.data.list[0].components;
-  } catch (error) {
-    console.error("Error fetching air quality data:", error);
-    return null;
-  }
-}
-
-function extractDataFromFile(filePath) {
-  try {
-    const fileContent = fs.readFileSync(filePath, "utf-8");
-    const lines = fileContent.split("\n");
-
-    const extractedData = {
-      Temperature: "none",
-      Pressure: "none",
-      Humidity: "none",
-      PM2_5: "none",
-      PM10: "none",
-    };
-
-    const header = lines[0].split(",");
-
-    if (header.includes("Sensor Code")) {
-      // Extract from Sensor Code type file
-      for (const line of lines) {
-        const parts = line.split(",");
-
-        if (parts.length >= 7 && parts[0] !== "Sensor Code") {
-          const [, so2, , co, o3, pm10, pm2_5] = parts;
-          extractedData.Temperature = "none"; // Not available in this type
-          extractedData.Pressure = "none"; // Not available in this type
-          extractedData.Humidity = "none"; // Not available in this type
-          extractedData.PM2_5 = pm2_5.trim() || extractedData.PM2_5;
-          extractedData.PM10 = pm10.trim() || extractedData.PM10;
-        }
-      }
-    } else if (header.includes("Temperature")) {
-      // Extract from Temperature type file
-      for (const line of lines) {
-        const parts = line.split(",");
-
-        if (parts.length >= 5 && parts[0] !== "Temperature") {
-          const [temperature, pressure, humidity, pm25, pm10] = parts;
-          extractedData.Temperature =
-            temperature.trim() || extractedData.Temperature;
-
-          // Divide pressure by 100 before storing it
-          //extractedData.Pressure =
-          //  (parseFloat(pressure) / 100).toString() || extractedData.Pressure;
-          extractedData.Pressure = pressure.trim() || extractedData.Pressure;
-          extractedData.Humidity = humidity.trim() || extractedData.Humidity;
-          extractedData.PM2_5 = pm25.trim() || extractedData.PM2_5;
-          extractedData.PM10 = pm10.trim() || extractedData.PM10;
-        }
-      }
+    if (fileStats.length > 0) {
+      return path.join(folderPath, fileStats[0].fileName);
+    } else {
+      return null;
     }
-
-    return extractedData;
   } catch (error) {
-    console.error("Error extracting data from file:", error);
+    console.error("Error getting newest JSON file:", error);
     return null;
   }
 }
-async function fetchDataForSearchQuery(searchQuery) {
-  const folderPaths = [
-    "cron-scraper/data/sensor_community",
-    "cron-scraper/data/CHMU/Česká republika",
-  ];
-  const fileExtension = ".csv";
+
+function fetchDataForSearchQuery(searchQuery) {
+  const jsonFolderPath = "cron-scraper/data/history/region_data"; // Update with the correct folder path
   const numResults = 15;
-  const excludedStrings = ["all-sensors", "all_stations"];
 
-  const location = "Czech Republic";
-  const temperatureData = await fetchTemperatureData(location);
-  if (!temperatureData) {
-    console.log("Failed to fetch temperature data.");
+  const newestJSONFilePath = getNewestJSONFile(jsonFolderPath);
+  if (!newestJSONFilePath) {
+    console.log("No JSON files found in the folder.");
     return [];
   }
 
-  const latitude = 50.0755;
-  const longitude = 14.4378;
-  const airQualityData = await fetchAirQualityData(latitude, longitude);
-  if (!airQualityData) {
-    console.log("Failed to fetch air quality data.");
+  const jsonData = readJSONData(newestJSONFilePath);
+  if (!jsonData) {
+    console.log("Failed to read JSON data.");
     return [];
   }
 
-  const averageTemperature = calculateAverage([temperatureData.temp]);
-  const averagePressure = calculateAverage([temperatureData.pressure]);
-  const averageHumidity = calculateAverage([temperatureData.humidity]);
-  const averagePM2_5 = calculateAverage([airQualityData.pm2_5]);
-  const averagePM10 = calculateAverage([airQualityData.pm10]);
-
-  const results = searchFiles(
-    searchQuery,
-    folderPaths,
-    fileExtension,
-    numResults,
-    excludedStrings
-  );
+  const results = performSearch(searchQuery, jsonData, numResults);
   console.log("Top Search Results:", results);
 
-  const finalData = [];
-
-  for (const result of results) {
-    for (const folderPath of folderPaths) {
-      const filePath = path.join(folderPath, result);
-      if (fs.existsSync(filePath)) {
-        console.log(`Found file: ${result}`);
-        const locationAddress = result.replace(".csv", "");
-        const extractedData = extractDataFromFile(filePath);
-
-        const openWeatherData = await fetchTemperatureData(locationAddress);
-
-        if (openWeatherData) {
-          // Only update if extracted data is not available
-          if (extractedData.Temperature === "none") {
-            extractedData.Temperature = openWeatherData.temp;
-          }
-          if (extractedData.Pressure === "none") {
-            extractedData.Pressure = openWeatherData.pressure;
-          }
-          if (extractedData.Humidity === "none") {
-            extractedData.Humidity = openWeatherData.humidity;
-          }
-        }
-
-        function updateValue(value, averageValue) {
-          return value === "none" || isNaN(parseFloat(value))
-            ? averageValue
-            : parseFloat(value);
-        }
-
-        extractedData.Temperature = updateValue(
-          extractedData.Temperature,
-          averageTemperature
-        );
-        extractedData.Pressure = updateValue(
-          extractedData.Pressure,
-          averagePressure
-        );
-        extractedData.Humidity = updateValue(
-          extractedData.Humidity,
-          averageHumidity
-        );
-        extractedData.PM2_5 = updateValue(extractedData.PM2_5, averagePM2_5);
-        extractedData.PM10 = updateValue(extractedData.PM10, averagePM10);
-
-        const finalExtractedData = {
-          Location: locationAddress,
-          Temperature: extractedData.Temperature,
-          Pressure: extractedData.Pressure,
-          Humidity: extractedData.Humidity,
-          PM2_5: extractedData.PM2_5,
-          PM10: extractedData.PM10,
-        };
-
-        console.log(`Final extracted data for ${result}:`, finalExtractedData);
-
-        finalData.push(finalExtractedData);
-      }
-    }
-  }
-
-  return finalData;
+  return results;
 }
 
 module.exports = fetchDataForSearchQuery;
