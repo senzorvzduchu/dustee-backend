279401f8d808dedec159357798c7717bec5ad0a8
unknown <Sasa-local@desktop-sasa.waage.cz>
2023-08-31 13:17:22 +0200

    installing requirements for cron jobs via pip

--- utils/cron-scheduler.js
+++ utils/cron-scheduler.js
@@ -2,6 +2,20 @@ const cron = require('node-cron');
 const { exec } = require('child_process');
 
 module.exports = {
+        //function for installing dependecies via PIP
+        runPIP(schedule) {
+                cron.schedule(schedule, () => {
+                        exec('pip install -r ../cron-scraper/requirements.txt', (error, stdout, stderr) => {
+                                if (error) {
+                                    console.error(`Chyba: ${error}`);
+                                    return;
+                                }
+                                console.log(`Výstup: ${stdout}`);
+                                console.error(`Chybový výstup: ${stderr}`);
+                        });
+                });
+        },
+
         //function for running CHMU.py script via CRON
         runCHMUpy(schedule) {
                 cron.schedule(schedule, () => {
